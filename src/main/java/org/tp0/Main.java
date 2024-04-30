782af280df5a26144a5a04745f211af8e952210c
ValentinaGottelli <valentinagottelli@gmail.com>
2024-04-30 01:35:12 -0300

    initial

--- src/main/java/org/tp0/Main.java
+++ src/main/java/org/tp0/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         Scanner scanner = new Scanner(System.in);
         int opcion;
 
@@ -27,6 +26,18 @@ public class Main {
             switch (opcion) {
                 case 1:
                     System.out.println("Eligio el Ejercicio 1");
+                    System.out.printf("");
+
+                    System.out.printf("Calculo de traza" + "\n");
+                    System.out.println(Ejercicio1.resolucionTraza());
+                    System.out.printf("");
+
+                    System.out.printf("Traspuesta de la matriz" + "\n");
+                    Ejercicio1.imprimirMatrix(Ejercicio1.traspuesta());
+                    System.out.printf("");
+
+                    System.out.printf("Suma matricial"+ "\n");
+                    Ejercicio1.imprimirMatrix(Ejercicio1.sumaMatrices());
 
                     break;
                 case 2:
@@ -50,7 +61,7 @@ public class Main {
 
                     break;
                 case 7:
-                    System.out.println("Exiting...");
+                    System.out.println("Cerrando...");
                     break;
                 default:
                     System.out.println("Invalid opcion. Please enter a number between 1 and 7.");
