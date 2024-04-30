ffde4cdbcbccfba2b7d86b6a9bc2766755dbcef8
ValentinaGottelli <valentinagottelli@gmail.com>
2024-04-30 00:39:51 -0300

    initial

---
+++ src/main/java/org/tp0/Main.java
@@ -0,0 +1,63 @@
+package org.tp0;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Ejercicio 1");
+            System.out.println("2. Ejercicio 2");
+            System.out.println("3. Ejercicio 3");
+            System.out.println("4. Ejercicio 4");
+            System.out.println("5. Ejercicio 5");
+            System.out.println("6. Ejercicio 6");
+            System.out.println("7. Exit");
+            System.out.print("Elija la opcion del 1 al 7: ");
+
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Eligio el Ejercicio 1");
+
+                    break;
+                case 2:
+                    System.out.println("Eligio el Ejercicio 2");
+
+                    break;
+                case 3:
+                    System.out.println("Eligio el Ejercicio 3");
+
+                    break;
+                case 4:
+                    System.out.println("Eligio el Ejercicio 4");
+
+                    break;
+                case 5:
+                    System.out.println("Eligio el Ejercicio 5");
+
+                    break;
+                case 6:
+                    System.out.println("Eligio el Ejercicio 6");
+
+                    break;
+                case 7:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid opcion. Please enter a number between 1 and 7.");
+            }
+        } while (opcion != 7);
+
+        scanner.close();
+
+    }
+}
---
+++ src/main/java/org/tp0/ej1.java
@@ -0,0 +1,8 @@
+package org.tp0;
+
+public class ej1 {
+
+    public void resolucion(){
+        System.out.printf("ola");
+    }
+}
